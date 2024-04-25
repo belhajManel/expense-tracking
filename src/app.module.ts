9864e873a7d53d99c7acc7dc96e4bfd1c4f1acd4
belhajManel <belhaj343@gmail.com>
2024-04-25 11:20:31 +0100

    Update dependencies and default config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { CategoriesModule } from './categories/categories.module';
+import { BudgetsModule } from './budgets/budgets.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/expense-tracking'),
+    UsersModule,
+    TransactionsModule,
+    CategoriesModule,
+    BudgetsModule,
+    ReportsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
