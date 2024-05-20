52c6e78c61ee98f6d27c8c82a31cb1dd7bd44572
oussamaB14 <88203283+oussamaB14@users.noreply.github.com>
2024-05-20 15:23:26 +0100

    updating planned expenses entity

--- src/planned_expenses/entities/planned_expense.entity.ts
+++ src/planned_expenses/entities/planned_expense.entity.ts
@@ -1,14 +1,14 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-//import { Document } from 'mongoose';
+import { Types } from 'mongoose';
+import { Document } from 'mongoose';
 @Schema({timestamps: true})
-export class PlannedExpense {
+export class PlannedExpense extends Document {
   @Prop({ required: true })
   amount: number;
   @Prop({ required: true })
   date: Date;
-  @Prop({ required: true })
-  category: string;
-
+  @Prop({ type: Types.ObjectId, ref: 'Category', required: true })
+  category: Types.ObjectId;
   @Prop({ required: true })
   description: string;
 }
